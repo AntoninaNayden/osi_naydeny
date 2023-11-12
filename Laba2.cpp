3a726ee86755f752772501d3f7faa926723ec61e
AntoninaNayden <121058964+AntoninaNayden@users.noreply.github.com>
2023-11-13 00:35:01 +0300

    Task 2

---
+++ Laba2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <processthreadsapi.h>
+#include <stdio.h>
+
+
+BOOL WINAPI ProcessEvents(DWORD ctrlType) {
+    switch (ctrlType)
+    {
+    case CTRL_BREAK_EVENT:std::cout << "Ctrl break event" << std::endl; break;
+
+    case CTRL_C_EVENT:std::cout << "Ctrl c event" << std::endl; break;
+
+    case CTRL_CLOSE_EVENT:std::cout << "Ctrl close event" << std::endl; break;
+
+    default:break;
+    }
+    return false;
+}
+
+void reverse_string() {
+    while (true) {
+        char str[100];
+        scanf_s("%s", str, 100);
+        std::cout << _strrev(str) << std::endl;
+    }
+}
+
+void random() {
+    while (true) {
+        double randomNum = (double)rand() / RAND_MAX;
+        std::cout << randomNum << std::endl;
+        Sleep(1000);
+    }
+}
+
+int main()
+{
+    SetConsoleCtrlHandler(ProcessEvents, 1);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)reverse_string, NULL, 0, NULL);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)random, NULL, 0, NULL);
+    while (true);
+    return 0;
+}
